602473ee27f17ed11708170bf63522532d7a7f3b
David Pizarro Palancar <dpizapal@yahoo.es>
2022-09-30 18:12:44 +0200

    uploading

---
+++ src/componentes/BotonClear.js
@@ -0,0 +1,13 @@
+import React from 'react'
+import '../hojas-de-estilo/BotonClear.css'
+
+export default function BotonClear(props) {
+  return (
+    <div 
+    className='boton-clear' 
+    onClick={props.manejarClear}
+    >
+      {props.children}
+    </div>
+  )
+}
